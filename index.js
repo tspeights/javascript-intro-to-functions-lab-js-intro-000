31c991a0f0f93ee41c18c28b70b1f9ce1ea7d61b
Trevor Speights <trevorspeights23@gmail.com>
2018-09-23 14:22:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ function sayHiToGrandma(string){
   return "I can't hear you!"
 }
   if (string.toUpperCase() === string){
-  return "YES INDEED!"
+    return "YES INDEED!"
 }
   if (string === "I love you, Grandma"){
-  return "I love you, too."
+    return "I love you, too."
 }
+
